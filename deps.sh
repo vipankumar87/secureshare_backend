41e08c34a0a55c079bc32abcdfcdce3b8bf17c84
Vipan Kumar <vipan.webdeveloper@gmail.com>
2024-10-03 17:48:26 +0530

    Init

---
+++ deps.sh
@@ -0,0 +1,25 @@
+#!/bin/bash
+
+cargo add argon2
+cargo add async-trait
+cargo add chrono --features serde
+cargo add dotenv
+cargo add jsonwebtoken
+cargo add serde --features derive
+cargo add serde_json
+cargo add sqlx --features runtime-async-std-native-tls,postgres,chrono,uuid
+cargo add uuid --features serde,v4
+cargo add validator --features derive
+cargo add axum --features multipart
+cargo add axum-extra --features cookie
+cargo add tokio --features full
+cargo add tokio-cron-scheduler
+cargo add tower
+cargo add time
+cargo add tower-http --features cors,trace
+cargo add tracing-subscriber
+cargo add aes
+cargo add block-modes
+cargo add rsa
+cargo add rand
+cargo add base64
